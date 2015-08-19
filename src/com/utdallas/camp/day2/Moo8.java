068d03841b7378f3a86786d0ed58f27c0240c295
Rahul Saraf <rahulsaraf@users.noreply.github.com>
2015-08-19 10:44:47 -0500

    Update Moo8.java

--- src/com/utdallas/camp/day2/Moo8.java
+++ src/com/utdallas/camp/day2/Moo8.java
@@ -50,59 +50,12 @@ public class Moo8 {
 		 
 			 Arrays.sort(orig_array, PhotoComparator);
 		 
-		/*int[] newArray = new int[noOfCows+1];
-		int count = 0;
-		boolean var = false;
-
-		for (int i = 0; i < orig_array.length - 1; i++) {
-			if(checkIfMatches(orig_array[i],orig_array[i+1],array)){
-				newArray[count] = orig_array[i];
-				newArray[count+ 1] = orig_array[i+1];
-				count++;
-				var = true;
-			}else if(var){
-				count++;
-				count++;
-				var = false;
-			}
-
-		}
-		
-*/		
+	
 	for (int i = 0; i < orig_array.length; i++) {
 			System.out.println(orig_array[i]);
 		}
 		
 		
-	}
-
-	private static boolean checkIfMatches(int prev, int next, int[][] array) {
-		int result = 0;
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length - 1; j++) {
-				if((array[i][j] == prev && array[i][j+1] == next)){
-					if(prev == 18 || next ==18){
-						System.out.println("here");
-					}
-					result = result + 1;
-				}
-			}
-		} 
-		
-		
-		
-		// || (array[i][j] == next && array[i][j+1] == prev)
-		
-		if(result > 2){
-			return true;
-		}else{
-			if(x == 0){
-				x = next;				
-			}else if(x == prev){
-				xFound = true;
-			}
-		}
-		return false;
 	}
 	
 	public static Comparator<Integer> PhotoComparator 
